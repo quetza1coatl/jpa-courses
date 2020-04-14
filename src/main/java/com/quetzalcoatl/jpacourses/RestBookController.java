7674f15add307f36705d7b2fb3a10b8cc70c4135
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2020-04-14 19:52:45 +0300

    HwSB simple REST

---
+++ src/main/java/com/quetzalcoatl/jpacourses/RestBookController.java
@@ -0,0 +1,18 @@
+package com.quetzalcoatl.jpacourses;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class RestBookController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(new Book(1, "JPA in pictures", "John Smith"),
+                new Book(2, "Booook", "OAuth"),
+                new Book(3, "JWT", "Ivan"));
+    }
+
+}
